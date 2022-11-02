195c8e8a0f9ec9ebc6c0f2552f9fbba133143f90
jay01122000 <110469644+jay01122000@users.noreply.github.com>
2022-11-02 13:08:23 +0530

    Add files via upload

--- display.php
+++ display.php
@@ -1,34 +1,34 @@
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.2/css/bootstrap.rtl.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.2/css/bootstrap.rtl.min.css"><br><br>
+<button class="btn btn-dark w-20"> <a href="insert.php">Add user </a></button>
 <div class="container mt-3">
-<button class="btn btn-primary"><a href="exam.php" class="text-white"> Add user </a> </button></br>
-<table class="table">
-	<tr> 
-	    <th>id </th>
-		 <th> name </th>
-		 <th> CONTECT </th>
-		 
-		 
-		 
+<table class="table font-monospace bg-dark text-info">
+	<tr> <th> id 
+		 <th>name
+		 <th>course
+		 <th>contact
+		 <th>update
+		 <th>delete
+	</tr>
 <?php
-	$con=new mysqli("localhost","root","","img");
-	$sql="SELECT * FROM `form`";
+$con=new mysqli("localhost","root","","register");
+	$sql="SELECT * FROM `admin`";
 	$result=mysqli_query($con,$sql);
-	if($result){
+	if($result)
+	{
 		while($row=mysqli_fetch_assoc($result))
 		{
-			$id=$row['id'];			
-			$name=$row['name'];			
-			$contect=$row['contect'];
-			
-			echo '<tr>
-				<td>'.$id.'</td>
-				<td>'.$name.'</td>
-				<td>'.$contect.'</td>
-    			<td> <button class="btn btn-info"><a href="update.php ? updateid='.$id.'"class="text-white">update</a></button>
-				<td> <button class="btn btn-danger"><a href="delete.php ? deleteid='.$id.'"class="text-white">delete</a></button>
-				</tr>';
-				
+		?>	
+			<tr> <td> <?php echo $row['id']; ?>
+			    <td> <?php echo $row['name']; ?>
+			    <td> <?php echo $row['coarse']; ?>
+			    <td> <?php echo $row['contect']; ?>
+				<td><button class="btn btn-info"><a href="update.php? id=<?php echo $row['id']; ?>">update</a></button>
+				<td><button class="btn btn-info"><a href="delete.php? id=<?php echo $row['id']; ?>">delete</a></button>
+								
+		<?php
 		}
 	}
+	
+
 ?>
+		 
